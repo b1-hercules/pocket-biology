8fe86ff812ce065252278652bdcb8dc6276d2920
firman <fkopitezz@gmail.com>
2024-07-04 06:56:06 +0700

    init

--- app/src/main/java/com/adi/mediapembelajaranmatematika/Activity/Materi.java
+++ app/src/main/java/com/adi/mediapembelajaranmatematika/Activity/Materi.java
@@ -1,25 +1,37 @@
 package com.adi.mediapembelajaranmatematika.Activity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.webkit.WebView;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.webkit.WebChromeClient;
 import com.adi.mediapembelajaranmatematika.R;
 
 public class Materi extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_materi);
 
+        Button btnOpenVideo = findViewById(R.id.btn_open_video);
         ImageView btnKembali = findViewById(R.id.btn_kembali);
         btnKembali.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 onBackPressed(); // Go back to the previous activity
+            }
+        });
+
+        btnOpenVideo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://www.youtube.com/watch?v=pRLzqHAWTcs&ab_channel=GIAAcademy"));
+                startActivity(intent);
             }
         });
 
