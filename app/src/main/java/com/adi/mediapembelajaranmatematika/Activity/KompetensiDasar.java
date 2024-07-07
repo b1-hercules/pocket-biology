3045de2d02aa3076da33cb14360fe863be2eb7be
firman <fkopitezz@gmail.com>
2024-07-07 10:27:06 +0700

    update : fixing keinginan mas qobul

--- app/src/main/java/com/adi/mediapembelajaranmatematika/Activity/KompetensiDasar.java
+++ app/src/main/java/com/adi/mediapembelajaranmatematika/Activity/KompetensiDasar.java
@@ -9,55 +9,15 @@ import android.view.View;
 import android.widget.ImageView;
 
 import com.adi.mediapembelajaranmatematika.R;
-import com.github.barteksc.pdfviewer.PDFView;
 
 public class KompetensiDasar extends AppCompatActivity {
 
     private ImageView btn_kembali;
-    private PDFView  pdfView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kompetensi_dasar);
-        pdfView =  findViewById(R.id.pdfView);
         init();
-        readPdf();
-    }
-
-    private void readPdf() {
-        pdfView.fromAsset("bahan_materi.pdf")
-                .pages(0, 2, 1, 3, 3, 3) // all pages are displayed by default
-                .enableSwipe(true) // allows to block changing pages using swipe
-                .swipeHorizontal(false)
-                .enableDoubletap(true)
-                .defaultPage(0)
-                // allows to draw something on the current page, usually visible in the middle of the screen
-              /*  .onDraw(onDrawListener)
-                // allows to draw something on all pages, separately for every page. Called only for visible pages
-                .onDrawAll(onDrawListener)
-                .onLoad(onLoadCompleteListener) // called after document is loaded and starts to be rendered
-                .onPageChange(onPageChangeListener)
-                .onPageScroll(onPageScrollListener)
-                .onError(onErrorListener)
-                .onPageError(onPageErrorListener)
-                .onRender(onRenderListener) // called after document is rendered for the first time
-                // called on single tap, return true if handled, false to toggle scroll handle visibility
-                .onTap(onTapListener)
-                .onLongPress(onLongPressListener)*/
-                .enableAnnotationRendering(false) // render annotations (such as comments, colors or forms)
-                .password(null)
-                .scrollHandle(null)
-                .enableAntialiasing(true) // improve rendering a little bit on low-res screens
-                // spacing between pages in dp. To define spacing color, set view background
-                .spacing(0)
-               // .autoSpacing(false) // add dynamic spacing to fit each page on its own on the screen
-             /*   .linkHandler(DefaultLinkHandler)
-                .pageFitPolicy(FitPolicy.WIDTH) // mode to fit pages in the view*/
-              //  .fitEachPage(false) // fit each page to the view, else smaller pages are scaled relative to largest page.
-             //   .pageSnap(false) // snap pages to screen boundaries
-               // .pageFling(false) // make a fling change only a single page like ViewPager
-               // .nightMode(false) // toggle night mode
-                .load();
     }
 
     private void init() {
--- app/src/main/java/com/adi/mediapembelajaranmatematika/Activity/Materi.java
+++ app/src/main/java/com/adi/mediapembelajaranmatematika/Activity/Materi.java
@@ -1,23 +1,40 @@
 package com.adi.mediapembelajaranmatematika.Activity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.ParcelFileDescriptor;
 import android.view.View;
 import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.webkit.WebChromeClient;
+import android.widget.LinearLayout;
+
 import com.adi.mediapembelajaranmatematika.R;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Materi extends AppCompatActivity {
+   // private PDFView pdfView;
+    private LinearLayout linearImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_materi);
 
-        Button btnOpenVideo = findViewById(R.id.btn_open_video);
+        linearImage = findViewById(R.id.linear_image);
+       // pdfView =  findViewById(R.id.pdfView);
+         Button btnOpenVideo = findViewById(R.id.btn_open_video);
         ImageView btnKembali = findViewById(R.id.btn_kembali);
         btnKembali.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,7 +43,7 @@ public class Materi extends AppCompatActivity {
             }
         });
 
-        btnOpenVideo.setOnClickListener(new View.OnClickListener() {
+       btnOpenVideo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Intent.ACTION_VIEW);
@@ -34,6 +51,7 @@ public class Materi extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
 
         WebView webView = findViewById(R.id.webview);
 
@@ -43,6 +61,32 @@ public class Materi extends AppCompatActivity {
         webView.getSettings().setJavaScriptEnabled(true);
         webView.setWebChromeClient(new WebChromeClient());
 
+     //   readPdf();
+        readImageAsset();
+    }
+
+    private void readImageAsset() {
+        for (int i = 0; i < 27; i++) {
+            AssetManager assetManager = getAssets();
+            ImageView imageView = new ImageView(this);
+            String name = String.valueOf(i+1);
+            try (
+                    //declaration of inputStream in try-with-resources statement will automatically close inputStream
+                    // ==> no explicit inputStream.close() in additional block finally {...} necessary
+                    InputStream inputStream = assetManager.open("materi/"+name+".jpg")
+            ) {
+                Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                imageView.setImageBitmap(bitmap);
+                imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+                imageView.setAdjustViewBounds(true);
+                imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+                linearImage.addView(imageView);
+            } catch (IOException ex) {
+                //ignored
+            }
+        }
+
 
     }
+
 }
